1405400a16d0b509d9a664d90ce755ac972e8b20
liuponghei <liuponghei@users.noreply.github.com>
2018-05-13 01:37:47 +0800

    Update xosp.mk

--- xosp.mk
+++ xosp.mk
@@ -15,9 +15,10 @@
 $(call inherit-product, device/asus/Z01B/full_Z01B.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Z01B
 PRODUCT_NAME := xosp_Z01B
+PRODUCT_DEVICE := Z01B
 PRODUCT_BRAND := XOSP
+PRODUCT_MODEL := Z01B
 PRODUCT_MANUFACTURER := asus
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.name
